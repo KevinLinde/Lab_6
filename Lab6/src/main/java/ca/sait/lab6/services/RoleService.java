3b509dfb1000bc57c76457b1aa6cf50590d378da
KevinLinde <75047430+KevinLinde@users.noreply.github.com>
2022-03-11 22:32:30 -0700

    Add files via upload

---
+++ Lab6/src/main/java/ca/sait/lab6/services/RoleService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sait.lab6.services;
+
+import ca.sait.lab6.dataaccess.UserDB;
+import ca.sait.lab6.dataaccess.RoleDB;
+import ca.sait.lab6.models.Role;
+import ca.sait.lab6.models.User;
+import java.util.List;
+
+/**
+ *
+ * @author Kevin
+ */
+public class RoleService {
+    private RoleDB roleDB = new RoleDB();
+    
+    
+    public List<Role> getAll() throws Exception {
+        List<Role> roles = roleDB.getAll();
+        return roles;
+    }
+    
+}
